31885e589faf0b7a089ef200e0fe1c5141a70774
Elise Erickson <elisece717@gmail.com>
2024-01-11 13:55:08 -0500

    makes form a controlled form

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function NewPlantForm() {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    price: 0
+  })
+
+  const onChangeForm = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
       <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+        <input type="text" name="name" placeholder="Plant name" value={formData.name} onChange={onChangeForm}/>
+        <input type="text" name="image" placeholder="Image URL" value={formData.image} onChange={onChangeForm}/>
+        <input type="number" name="price" step="0.01" placeholder="Price" value={formData.price} onChange={onChangeForm}/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
